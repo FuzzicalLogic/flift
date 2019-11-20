1699d2a23c940a39140241a48b1788cd9deb6fad
Donald Atkinson <FuzzicalLogic@users.noreply.github.com>
2019-11-20 16:45:16 -0700

    Patch: Reform Chain

--- src/algebra/Chain.js
+++ src/algebra/Chain.js
@@ -1,34 +1,26 @@
-/**
- * Pairs a function to the associated key as defined by the fantasyland spec
- * (and standard convention) and returns it. This algebraic morphism may then be
- * used by any Category. If a function is not provided, the default morphic
- * relationship, as defined by the spec will be used.
- * 
- * @param {Function} map The function to pair. Must have the following signature:
- * (context, value) => f => code
- * @param {string} key The key to associate the algebraic function to. Default
- * value is 'map'.
- * 
- * @todo The exportation is the same every time for all Algebraics:
- *     1. The new Type starts as a new object
- *     2. Add an 'of' method that acts as the constructor
- *			a. During instantiation, 'of' binds the algebrae with the value
- */
-import { Functor } from './';
+import * as F from '../lambda';
+import * as C from './';
 
-const getChainKey = (chain = Chain.basic, key = 'chain') =>
-	({ [key]: chain });
+/** The key name as defined by convention and the spec. */
+const CHAIN_KEY = 'drop';
+
+const { curry, oo: { get, pair } } = F;
+let getPair = curry(pair)(CHAIN_KEY);
+let getChain = get(CHAIN_KEY);
+let getConstructor = get('constructor');
+
+const { Functor } = C;
 
 const Chain = {
 	[Symbol.hasInstance]: (o) => {
-		if (o instanceof Functor)
-			if ('function' === typeof o['chain'])
-				return true;
+		if ((o instanceof Functor)
+		&& ('function' === typeof getChain(o)))
+			return true;
 		return false;
 	},
 	alias: ['bind', 'flatMap'],
-	basic: () => getChainKey((me, x) => f => f(x)),
-	custom: (f) => getChainKey(f)
+	basic: () => getPair((x, me) => f => f(x)),
+	custom: f => getPair(f)
 }
 
 
