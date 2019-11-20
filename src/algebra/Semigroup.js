81764df35fddb9a1692c5d942c0ef84470675332
Donald Atkinson <FuzzicalLogic@users.noreply.github.com>
2019-11-20 09:13:27 -0700

    Patch: Reformat Semigroup

--- src/algebra/Semigroup.js
+++ src/algebra/Semigroup.js
@@ -1,19 +1,31 @@
-import { pipeSync } from '../lambda';
+import * as F from '../lambda';
+import * as C from './';
 
-/**
- * Creates a new Functor Type from a function. A Functor is the most basic of
- * the algebrae and when mapped will automatically recontextualize the result
- * to a new instance of the Type.
- * 
- * @param {any} fMap The function that is used to map any function to the value.
- */
-export const Semigroup = (concat) => {
-	let algebra = {
-		concat: concat
-	}
-	let exported = x => ({
-		concat: pipeSync(algebra.concat(x), exported)
-	});
-	exported.algebra = algebra;
-	return exported;
+/** The key name as defined by convention and the spec. */
+const SEMIGROUP_KEY = 'concat';
+
+const { curry, get, oo: { pair } } = F;
+let getPair = curry(pair)(SEMIGROUP_KEY);
+let getConcat = get(SEMIGROUP_KEY);
+let getConstructor = get('constructor');
+
+const { Droppable } = C;
+const Semigroup = {
+    [Symbol.hasInstance]: o => {
+        if ((o instanceof Droppable)
+        && ('function' === typeof getConcat(o))
+        && (getConstructor(o) === getConstructor(getConcat(o)(x))))
+            return true;
+        return false;
+    },
+
+    /**
+     * Creates a new Functor Type from a function. A Functor is the most basic 
+     * of the algebrae and when mapped will automatically recontextualize the result
+     * to a new instance of the Type.
+     *
+     * @param {Function} f The function that is used to concatenate to the 
+     * Semigroup.
+     */
+    from: f => getPair(f),
 };
