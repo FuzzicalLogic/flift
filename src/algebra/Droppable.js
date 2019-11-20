fa95a86a6870ab3358c6bb7de145e5e896b0b314
Donald Atkinson <FuzzicalLogic@users.noreply.github.com>
2019-11-20 09:26:57 -0700

    Patch: Reform Droppable

--- src/algebra/Droppable.js
+++ src/algebra/Droppable.js
@@ -1,15 +1,21 @@
-const getDroppableKey = (drop = Droppable.basic, key = 'drop') =>
-	({ [key]: drop });
+import * as F from '../lambda';
+
+/** The key name as defined by convention and the spec. */
+const DROP_KEY = 'map';
+
+const { curry, get, oo: { pair } } = F;
+let getPair = curry(pair)(DROP_KEY);
+let getDrop = get(DROP_KEY);
 
 const Droppable = {
 	[Symbol.hasInstance]: (o) => {
-		if ('function' === typeof o['drop'])
+		if ('function' === typeof getDrop(o))
 			return true;
 		return false;
 	},
-	alias: ['cata', 'fold'],
-	basic: () => getDroppableKey((me, x) => f => f(x)),
-	custom: f => getDroppableKey(f)
+    alias: ['cata', 'fold'],
+	basic: () => getPair((me, x) => f => f(x)),
+    custom: f => getPair(f)
 }
 
 export { Droppable };
